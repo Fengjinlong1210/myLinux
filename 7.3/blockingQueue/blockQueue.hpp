3664c6d6b713b36bbea9e6f176297f7e1583e7f7
Fengjinlong1210 <239383772@qq.com>
2023-07-04 19:55:45 +0800

    生产消费模型-多对多

--- 7.3/blockingQueue/blockQueue.hpp
+++ 7.3/blockingQueue/blockQueue.hpp
@@ -1,86 +1,158 @@
-#pragma once
-#include <iostream>
-#include <pthread.h>
-#include <unistd.h>
-#include <queue>
+// #pragma once
+// #include <iostream>
+// #include <pthread.h>
+// #include <unistd.h>
+// #include <queue>
 
+// template <class T>
+// class BlockQueue
+// {
+// public:
+//     BlockQueue(const int capacity = 5)
+//         : _capacity(capacity)
+//     {
+//         pthread_mutex_init(&_mutex, nullptr);
+//         pthread_cond_init(&_consumer, nullptr);
+//         pthread_cond_init(&_producer, nullptr);
+//     }
+
+//     void push(const T &val)
+//     {
+//         // 生产者push的时候, 要保证队列不为空
+//         // 先申请锁
+//         pthread_mutex_lock(&_mutex);
+//         //进入临界区, 当前持有锁
+//         //等待环境变量
+//         while(isFull())    //使用while循环, 保证在任何时候都是要符合条件才进行生产
+//         {
+//             //要让线程等待条件允许, 不能持有锁等待
+//             //所以条件变量等待要传入锁, 来释放锁
+//             pthread_cond_wait(&_producer, &_mutex);
+//             //当线程被唤醒后, 继续从wait处向后执行
+//             //需要重新申请锁, 申请成功才会彻底返回
+//         }
+//         //出循环是, 队列不满, 可以进行生产
+//         //可以加条件控制队列数量
+
+//         _queue.push(val);
+//         //push后, 通知消费者可以进行获取队列内容
+//         pthread_cond_signal(&_consumer);
+//         pthread_mutex_unlock(&_mutex);
+//         //也可在释放锁之后通知, 没有区别, 因为消费者和生产者是互斥的
+//     }
+
+//     void pop(T *val)
+//     {
+//         // 消费者pop获取数据的时候, 要保证队列中有数据
+//         pthread_mutex_lock(&_mutex);
+
+//         while(isEmpty())
+//         {
+//             //等待生产者通知
+//             pthread_cond_wait(&_consumer, &_mutex);
+//         }
+//         //输出型参数
+//         *val = _queue.front();
+//         _queue.pop();
+//         //通知生产者可以继续生产
+//         pthread_cond_signal(&_consumer);
+//         pthread_mutex_unlock(&_mutex);
+//     }
+
+//     bool isFull()
+//     {
+//         return _queue.size() == _capacity;
+//     }
+
+//     bool isEmpty()
+//     {
+//         return _queue.empty();
+//     }
+
+//     ~BlockQueue()
+//     {
+//         pthread_mutex_destroy(&_mutex);
+//         pthread_cond_destroy(&_consumer);
+//         pthread_cond_destroy(&_producer);
+//     }
+
+// private:
+//     // 一个锁、两个条件变量、一个队列
+//     std::queue<T> _queue;
+//     pthread_mutex_t _mutex;
+//     //一把锁是因为队列被当成一份资源来使用
+//     pthread_cond_t _consumer;
+//     pthread_cond_t _producer;
+//     size_t _capacity;
+// };
+
+#pragma once
+
+#include <iostream>
+#include <queue>
+#include <pthread.h>
+
+const int gcap = 5;
+
+// 不要认为，阻塞队列只能放整数字符串之类的
+// 也可以放对象
 template <class T>
 class BlockQueue
 {
 public:
-    BlockQueue(const int capacity = 5)
-        : _capacity(capacity)
+    BlockQueue(const int cap = gcap):_cap(cap)
     {
         pthread_mutex_init(&_mutex, nullptr);
-        pthread_cond_init(&_consumer, nullptr);
-        pthread_cond_init(&_producer, nullptr);
+        pthread_cond_init(&_consumerCond, nullptr);
+        pthread_cond_init(&_productorCond, nullptr);
     }
-
-    void push(const T &val)
+    bool isFull(){ return _q.size() == _cap; }
+    bool isEmpty() { return _q.empty(); }
+    void push(const T &in)
     {
-        // 生产者push的时候, 要保证队列不为空
-        // 先申请锁
         pthread_mutex_lock(&_mutex);
-        //进入临界区, 当前持有锁
-        //等待环境变量
-        while(isFull())    //使用while循环, 保证在任何时候都是要符合条件才进行生产
+        // 细节1：一定要保证，在任何时候，都是符合条件，才进行生产
+        while(isFull()) // 1. 我们只能在临界区内部，判断临界资源是否就绪！注定了我们在当前一定是持有锁的！
         {
-            //要让线程等待条件允许, 不能持有锁等待
-            //所以条件变量等待要传入锁, 来释放锁
-            pthread_cond_wait(&_producer, &_mutex);
-            //当线程被唤醒后, 继续从wait处向后执行
-            //需要重新申请锁, 申请成功才会彻底返回
+            // 2. 要让线程进行休眠等待，不能持有锁等待！
+            // 3. 注定了，pthread_cond_wait要有锁的释放的能力！
+            pthread_cond_wait(&_productorCond, &_mutex); // 我休眠(切换)了，我醒来的时候，在哪里往后执行呢？
+            // 4. 当线程醒来的时候，注定了继续从临界区内部继续运行！因为我是在临界区被切走的！
+            // 5. 注定了当线程被唤醒的时候，继续在pthread_cond_wait函数出向后运行，又要重新申请锁，申请成功才会彻底返回
         }
-        //出循环是, 队列不满, 可以进行生产
-        //可以加条件控制队列数量
-
-        _queue.push(val);
-        //push后, 通知消费者可以进行获取队列内容
-        pthread_cond_signal(&_consumer);
+        // 没有满的，就要让他进行生产
+        _q.push(in);
+        // 加策略
+        // if(_q.size() >= _cap/2) 
+        pthread_cond_signal(&_consumerCond);
         pthread_mutex_unlock(&_mutex);
-        //也可在释放锁之后通知, 没有区别, 因为消费者和生产者是互斥的
+        // pthread_cond_signal(&_consumerCond);
     }
-
-    void pop(T *val)
+    void pop(T *out)
     {
-        // 消费者pop获取数据的时候, 要保证队列中有数据
         pthread_mutex_lock(&_mutex);
-
-        while(isEmpty())
+        while(isEmpty()) 
         {
-            //等待生产者通知
-            pthread_cond_wait(&_consumer, &_mutex);
+            pthread_cond_wait(&_consumerCond, &_mutex);
         }
-        //输出型参数
-        *val = _queue.front();
-        _queue.pop();
-        //通知生产者可以继续生产
+        *out = _q.front();
+        _q.pop();
+        // 加策略
+        pthread_cond_signal(&_productorCond);
         pthread_mutex_unlock(&_mutex);
     }
-
-    bool isFull()
-    {
-        return _queue.size() == _capacity;
-    }
-
-    bool isEmpty()
-    {
-        return _queue.empty();
-    }
-
     ~BlockQueue()
     {
         pthread_mutex_destroy(&_mutex);
-        pthread_cond_destroy(&_consumer);
-        pthread_cond_destroy(&_producer);
+        pthread_cond_destroy(&_consumerCond);
+        pthread_cond_destroy(&_productorCond);
     }
-
 private:
-    // 一个锁、两个条件变量、一个队列
-    std::queue<T> _queue;
-    pthread_mutex_t _mutex;
-    //一把锁是因为队列被当成一份资源来使用
-    pthread_cond_t _consumer;
-    pthread_cond_t _producer;
-    size_t _capacity;
+    std::queue<T> _q;
+    int _cap;
+    // 为什么我们的这份代码，只用一把锁呢，根本原因在于，
+    // 我们生产和消费访问的是同一个queue&&queue被当做整体使用！
+    pthread_mutex_t _mutex; 
+    pthread_cond_t _consumerCond;   // 消费者对应的条件变量，空，wait
+    pthread_cond_t _productorCond;  // 生产者对应的条件变量，满，wait
 };
--- 7.3/blockingQueue/cp.cc
+++ 7.3/blockingQueue/cp.cc
@@ -2,45 +2,68 @@
 #include <unistd.h>
 #include <ctime>
 #include <pthread.h>
+#include "task.hpp"
 #include "blockQueue.hpp"
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 using namespace std;
 
 void *consumer_func(void *args)
 {
-    BlockQueue<int> *bq = static_cast<BlockQueue<int> *>(args);
+    BlockQueue<Task> *bq = static_cast<BlockQueue<Task> *>(args);
     while (1)
     {
-        int *pdata = new int;
-        bq->pop(pdata);
-        cout << "consumer get :" << *pdata << endl;
-        sleep(1);
+        Task t;
+        bq->pop(&t);
+        t();//执行operator()获得结果
+        cout << "consumer: " << t.formatArgs() << t.formatResult() << endl;
     }
 }
 
 void *producer_func(void *args)
 {
-    BlockQueue<int> *bq = static_cast<BlockQueue<int> *>(args);
+    BlockQueue<Task> *bq = static_cast<BlockQueue<Task> *>(args);
+    string str_op = "+-*/%";
     while (1)
     {
-        int data = rand() % 10;
-        bq->push(data);
-        cout << "producer push : " << data << endl;
-        sleep(1);
+        int num1 = rand() % 20 + 1;
+        int num2 = rand() % 10 + 1;
+        char op = str_op[rand() % str_op.size()];
+        Task t(num1, num2, op);
+        bq->push(t);
+        cout << "producer: " << t.formatArgs() << "?" << endl;
+        sleep(3m);
     }
 }
 
 int main()
 {
     srand((unsigned int)time(nullptr));
-    // 创造单对单的CP模型
-    BlockQueue<int> *bq = new BlockQueue<int>();
-    pthread_t consumer, producer;
+    //多对多的模型
+    BlockQueue<Task> *bq = new BlockQueue<Task>(20);
+    pthread_t c[3], p[3];
+    pthread_create(c + 0, nullptr, consumer_func, bq);
+    pthread_create(c + 1, nullptr, consumer_func, bq);
+    pthread_create(c + 2, nullptr, consumer_func, bq);
+    pthread_create(p + 0, nullptr, producer_func, bq);
+    pthread_create(p + 1, nullptr, producer_func, bq);
+    pthread_create(p + 2, nullptr, producer_func, bq);
 
-    pthread_create(&consumer, nullptr, consumer_func, bq);
-    pthread_create(&producer, nullptr, producer_func, bq);
+    pthread_join(c[0], nullptr);
+    pthread_join(c[1], nullptr);
+    pthread_join(c[2], nullptr);
+    pthread_join(p[0], nullptr);
+    pthread_join(p[1], nullptr);
+    pthread_join(p[2], nullptr);
 
-    pthread_join(consumer, nullptr);
-    pthread_join(producer, nullptr);
+    // // 创造单对单的CP模型
+    // BlockQueue<Task> *bq = new BlockQueue<Task>(20);
+
+    // pthread_t consumer, producer;
+
+    // pthread_create(&consumer, nullptr, consumer_func, bq);
+    // pthread_create(&producer, nullptr, producer_func, bq);
+
+    // pthread_join(consumer, nullptr);
+    // pthread_join(producer, nullptr);
     return 0;
 }
